06d06dbac5c28b79f38d1d1ec022759996a972fa
Hassan Ul Haq <81625175+Hassanulhaq52@users.noreply.github.com>
2022-08-23 17:26:47 +0500

    AppBar and Image Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() =>
+void main() {
   runApp(
-  const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        appBar: AppBar(
+          title: const Text('I am Rich'),
+          backgroundColor: Colors.indigo,
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+                'https://media.gettyimages.com/photos/quaideazam-picture-id184944186?s=612x612'),
+          ),
+        ),
       ),
     ),
   );
-
+}
